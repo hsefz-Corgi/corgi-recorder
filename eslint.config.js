c1ec16e50486459cc3e41c23f2b02eacb6d31f42
hsefz-lihugang <189581511+hsefz-lihugang@users.noreply.github.com>
2024-12-06 23:03:54 +0800

    initialize commit

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+
+    {
+        name: 'app/files-to-ignore',
+        ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    },
+
+    {
+        rules: {
+            "spaces": 4,
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+            "quotes": ["error", "single"]
+        }
+    },
+
+    ...pluginVue.configs['flat/essential'],
+    ...vueTsEslintConfig(),
+    skipFormatting,
+]
