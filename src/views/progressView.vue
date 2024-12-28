2a1a95b988c57a64b234e009f572e6688bb9cee2
hsefz-lihugang <zz.lihugang@hsefz.cn>
2024-12-28 18:23:02 +0800

    Dev-UI: Modify the color of the progress bar of converting videos from blue to green because of cyy's vexatious requirement

--- src/views/progressView.vue
+++ src/views/progressView.vue
@@ -1,7 +1,9 @@
 <template>
     <n-space class="p-4 select-none" :vertical="true">
         正在导出文件... (已完成 {{ currentProgress }}%)
-        <n-progress type="line" :percentage="currentProgress" indicator-placement="inside" processing /> </n-space>
+        <n-progress type="line" :percentage="currentProgress" indicator-placement="inside" processing
+            status="success" />
+    </n-space>
 </template>
 <script lang="ts" setup>
 import { NSpace, NProgress } from 'naive-ui';
