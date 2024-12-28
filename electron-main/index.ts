315b830c1957ee8bf9602eb7e22e0faa691fe9c0
hsefz-lihugang <zz.lihugang@hsefz.cn>
2024-12-28 16:22:46 +0800

    Dev: add forgetting to terminate recording notification

--- electron-main/index.ts
+++ electron-main/index.ts
@@ -16,6 +16,8 @@ import { registerAudioInfoFetcherIpcHandler } from './record/getAudioInfo';
 
 import { cleanupFlv, registerRecorderIpc } from './record/record';
 
+import startForgetToTerminateDetector from './utils/notification/forgetToTerminate';
+
 if (!app.requestSingleInstanceLock()) app.quit();
 
 import { logger } from './utils/configureLog';
@@ -60,6 +62,9 @@ Promise.all([
     registerAudioTestIpcHandler();
     registerAudioInfoFetcherIpcHandler();
     logger.info('IPC registered.');
+
+    startForgetToTerminateDetector();
+    logger.info('Task registered.');
 
     if (await isFirstRun()) {
         await startSetupWindow();
