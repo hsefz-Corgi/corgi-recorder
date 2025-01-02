bba1f52e91ceabd518580e93272474f94200e78c
hsefz-ChenJunJie <zz.chenjunjie1@hsefz.cn>
2025-01-02 08:13:45 +0800

    Patch - UI: update color of closeButton and title focus of errorView

--- src/views/errorView.vue
+++ src/views/errorView.vue
@@ -1,26 +1,36 @@
 <template>
     <div class="bg-white border-gray-400 border-b-gray-300 border p-1 select-none pb-2 pl-2 pr-1">
         <div class="title">
-            <div class=" text-sm text-gray-400 font-thin">{{ appName }}</div>
-            <div style="position: fixed; right: 0px; top: 0px; padding-left: 4px; padding-right: 4px"
-                class="close hover:bg-red-600 cursor-pointer" @click="close">
-                <span class="pr-1.5 pl-1.5 text-xl">×</span>
+            <div :class="['text-sm', isWindowFocused ? 'text-black' : 'text-gray-400']">
+                {{ appName }}
+            </div>
+            <div
+                style="position: fixed; right: 0px; top: 0px; padding-left: 4px; padding-right: 4px"
+                class="close hover:bg-[#e81123] cursor-pointer"
+                @click="close"
+            >
+                <span class="pr-2 pl-2 text-xl">×</span>
             </div>
         </div>
-        <div>
-            <div class="bg-white pl-2 pr-2 pt-3 pb-3">
-                <div class="pl-4 pr-4 pt-4 text-base">{{ content }}</div>
-            </div>
+        <div class="bg-white pl-2 pr-2 pt-3 pb-3">
+            <div class="pl-4 pr-4 pt-4 text-base">{{ content }}</div>
         </div>
     </div>
-    <div class="bg-gray-100 border-gray-400 border-l border-r border-b pl-2 pr-2 pt-2 pb-2 text-right">
-        <button @click="close"
+    <div
+        class="bg-gray-100 border-gray-400 border-l border-r border-b pl-2 pr-2 pt-2 pb-2 text-right"
+    >
+        <button
+            @click="close"
             class="focus:outline-none border border-blue-400 hover:border-blue-300 select-none pl-4 pr-4 text-sm hover:bg-radial-gradient"
-            autofocus @keydown.enter="close" ref="confirmButton">确定</button>
+            autofocus
+            @keydown.enter="close"
+            ref="confirmButton"
+        >
+            确定
+        </button>
     </div>
 
     <audio src="assets/sounds/error.mp3" class="hidden" ref="error"></audio>
-
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
@@ -31,10 +41,12 @@ const content = ref('');
 const id = ref(0);
 const bridge = getBridge();
 
+const isWindowFocused = ref(true);
+
 const error = ref<HTMLAudioElement>();
 const confirmButton = ref<HTMLButtonElement>();
 
-bridge.config.get().then(config => {
+bridge.config.get().then((config) => {
     appName.value = config.software.title;
 });
 
@@ -49,6 +61,12 @@ const close = () => bridge.page.error.close(id.value);
     }
 };
 
+window.addEventListener('focus', () => {
+    isWindowFocused.value = true;
+});
+window.addEventListener('blur', () => {
+    isWindowFocused.value = false;
+});
 </script>
 <style scoped>
 * {
